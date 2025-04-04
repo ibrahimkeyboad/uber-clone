30fbfe6cbda9ff06255095dfdb7af85dba1f24de
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-04 11:00:42 +0300

    add authentication redirect logic to index page based on user sign-in status

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,12 @@
+import { useAuth } from '@clerk/clerk-expo';
 import { Redirect } from 'expo-router';
-import { Text } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
+  const { isSignedIn } = useAuth();
+
+  if (isSignedIn) {
+    return <Redirect href="/home" />;
+  }
+
   return <Redirect href="/(auth)/welcome" />;
 }
