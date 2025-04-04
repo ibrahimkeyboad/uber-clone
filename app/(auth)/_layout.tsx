5228391420f0a0f454b25f89ab85fb34b8a8e21b
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-04 11:01:02 +0300

    rename AuthRoot component to AuthRoutesLayout for clarity

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,6 +1,6 @@
 import { Stack } from 'expo-router';
 
-export default function AuthRoot() {
+export default function AuthRoutesLayout() {
   return (
     <Stack>
       <Stack.Screen name="welcome" options={{ headerShown: false }} />
