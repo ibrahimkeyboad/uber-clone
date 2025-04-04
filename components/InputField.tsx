19ad560167793b5d64ad1af15fb7c41ce58ad71f
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-04 11:02:43 +0300

    refactor InputField component for improved styling and accessibility

--- components/InputField.tsx
+++ components/InputField.tsx
@@ -26,12 +26,13 @@ export default function InputField({
     <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View className="m-2 w-full">
-          <Text className={`font-JakartaSemiBold mb-3 text-lg ${labelStyle}`}>{label}</Text>
+          <Text className={`mb-3 font-JakartaSemiBold text-lg ${labelStyle}`}>{label}</Text>
           <View
-            className={`focus:border-primary-500  relative flex-row items-center justify-start rounded-full border border-neutral-100 bg-neutral-100 ${containerStyle}`}>
+            className={`relative  flex-row items-center justify-start rounded-full border border-neutral-100 bg-neutral-100 focus:border-primary-500 ${containerStyle}`}>
             {icon && <Image source={icon} className={`ml-4 size-5 ${iconStyle}`} />}
             <TextInput
-              className={`font-JakartaSemiBold flex-1 rounded-full p-4 text-[15px] ${inputStyle} text-left`}
+              className={`flex-1 rounded-full p-4 font-JakartaSemiBold text-[15px] ${inputStyle} text-left`}
+              placeholderTextColor="rgb(209 213 219 / 0.3)"
               secureTextEntry={secureTextEntry}
               {...props}
             />
