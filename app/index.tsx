36646aa1819d50fa88ed2aa58feb9a19580f3368
ibrahimkeyboad <ibrahimmwanga07@gmail.com>
2025-04-05 13:26:21 +0300

    add loading indicator and update redirect path for signed-in users

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
 import { useAuth } from '@clerk/clerk-expo';
 import { Redirect } from 'expo-router';
+import { ActivityIndicator } from 'react-native';
 
 export default function Index() {
-  const { isSignedIn } = useAuth();
+  const { isSignedIn, isLoaded } = useAuth();
+
+  if (!isLoaded) {
+    return <ActivityIndicator />;
+  }
+
+  console.log('isSignedIn', isSignedIn);
 
   if (isSignedIn) {
-    return <Redirect href="/home" />;
+    return <Redirect href="/(root)/(tabs)/home" />;
   }
 
   return <Redirect href="/(auth)/welcome" />;
